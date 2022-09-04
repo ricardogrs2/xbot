3f77297bc0711db7fcd3d5ef5df87db9004cdbaf
ricardogrs2 <ricardogrs2@gmail.com>
2022-09-04 14:08:44 +0000

    Ajustes do disparo

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const client = new Client({
     authStrategy: new LocalAuth()
 });
 const port = 3000;
-
+const app = express();
  
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
@@ -17,3 +17,77 @@ client.on('ready', () => {
 });
 
 client.initialize();
+
+// POST send-message
+app.post('/send-message', async (req, res) => {
+  console.log(req);
+  const sender = req.body.sender;
+  const number = req.body.number;
+  const message = req.body.message;
+  const numberDDI = number.substr(0, 2);
+  const numberDDD = number.substr(2, 2);
+  const numberUser = number.substr(-8, 8);
+
+  const client = sessions.find(sess => sess.id == sender)?.client;
+
+  if (!client) {
+    return res.status(422).json({
+      status: false,
+      message: `Sender: ${sender} não foi encontrado!`
+    })
+  }
+
+  if (numberDDI !== "55") {
+    const numberZDG = number + "@c.us";
+    client.sendMessage(numberZDG, message).then(response => {
+    res.status(200).json({
+      status: true,
+      message: 'BOT-ZDG Mensagem enviada',
+      response: response
+    });
+    }).catch(err => {
+    res.status(500).json({
+      status: false,
+      message: 'BOT-ZDG Mensagem não enviada',
+      response: err.text
+    });
+    });
+  }
+  else if (numberDDI === "55" && parseInt(numberDDD) <= 30) {
+    const numberZDG = "55" + numberDDD + "9" + numberUser + "@c.us";
+    client.sendMessage(numberZDG, message).then(response => {
+    res.status(200).json({
+      status: true,
+      message: 'BOT-ZDG Mensagem enviada',
+      response: response
+    });
+    }).catch(err => {
+    res.status(500).json({
+      status: false,
+      message: 'BOT-ZDG Mensagem não enviada',
+      response: err.text
+    });
+    });
+  }
+  else if (numberDDI === "55" && parseInt(numberDDD) > 30) {
+    const numberZDG = "55" + numberDDD + numberUser + "@c.us";
+    client.sendMessage(numberZDG, message).then(response => {
+    res.status(200).json({
+      status: true,
+      message: 'BOT-ZDG Mensagem enviada',
+      response: response
+    });
+    }).catch(err => {
+    res.status(500).json({
+      status: false,
+      message: 'BOT-ZDG Mensagem não enviada',
+      response: err.text
+    });
+    });
+  }
+});
+
+
+router.listen(port, () => {
+        console.log(`Api started at the port: ${port}`);
+});
