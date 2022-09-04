525c3f147c30c977a03ae0e8c480fb587af37e4c
ricardogrs2 <ricardogrs2@gmail.com>
2022-09-04 03:06:51 +0000

    Adição do express

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const qrcode = require('qrcode-terminal');
-
+const express = require('express');
 const { Client, LocalAuth } = require('whatsapp-web.js');
-
+const router = express();
 const client = new Client({
     authStrategy: new LocalAuth()
 });
-
- 
+const port = 3000;
 
  
 client.on('qr', qr => {
