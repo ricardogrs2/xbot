f8a73821afbc6e6ded14e6e4e9896087278a93fb
ricardogrs2 <ricardogrs2@gmail.com>
2022-09-05 13:39:14 +0000

    Funções de validação de números sem espaços

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const qrcode = require('qrcode-terminal');
 const express = require('express');
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const router = express();
+function apenasNumeros(str) {
+    str = typeof str.toString();
+    return str.replace(/\D+/g, "");
+}
+//
+function soNumeros(string) {
+    var numsStr = String(string).replace(/\D+/g, "");
+    return parseInt(numsStr);
+}
 const client = new Client({
     authStrategy: new LocalAuth()
 });
 const port = 3000;
-const app = express(); 
+const app = express();
+var clientQrcode;
 client.on('qr', qr => {
+    clientQrcode = qr;
     qrcode.generate(qr, {small: true});
 });
 
@@ -17,6 +28,64 @@ client.on('ready', () => {
 
 client.initialize();
 
+router.post('/send-message', async (req, res) => {
+  console.log(req);
+  const number = req.body.number;
+  const message = req.body.message;
+  const numberDDI = number.substr(0, 2);
+  const numberDDD = number.substr(2, 2);
+  const numberUser = number.substr(-8, 8);
+
+  if (numberDDI !== "55") {
+    const numberZDG = number + "@c.us";
+    client.sendMessage(numberZDG, message).then(response => {
+    res.status(200).json({
+      status: true,
+      message: 'BOT-ZDG Mensagem enviada',
+      response: response
+    });
+    }).catch(err => {
+    res.status(500).json({
+      status: false,
+      message: 'BOT-ZDG Mensagem não enviada',
+      response: err.text
+    });
+    });
+  }
+  else if (numberDDI === "55" && parseInt(numberDDD) <= 30) {
+    const numberZDG = "55" + numberDDD + "9" + numberUser + "@c.us";
+    client.sendMessage(numberZDG, message).then(response => {
+    res.status(200).json({
+      status: true,
+      message: 'BOT-ZDG Mensagem enviada',
+      response: response
+    });
+    }).catch(err => {
+    res.status(500).json({
+      status: false,
+      message: 'BOT-ZDG Mensagem não enviada',
+      response: err.text
+    });
+    });
+  }
+  else if (numberDDI === "55" && parseInt(numberDDD) > 30) {
+    const numberZDG = "55" + numberDDD + numberUser + "@c.us";
+    client.sendMessage(numberZDG, message).then(response => {
+    res.status(200).json({
+      status: true,
+      message: 'BOT-ZDG Mensagem enviada',
+      response: response
+    });
+    }).catch(err => {
+    res.status(500).json({
+      status: false,
+      message: 'BOT-ZDG Mensagem não enviada',
+      response: err.text
+    });
+    });
+  }
+});
+
 router.get("/validacel", async (req, res, next) => {
     let chatId = req.query.numero;
     var result = await client.isRegisteredUser(chatId + '@c.us');
@@ -25,6 +94,21 @@ router.get("/validacel", async (req, res, next) => {
     }
     return res.json(result);
 
+});
+router.get("/status", async (req, res, next) => {
+    var result = await client.getState();
+    if (!result) {
+            return res.json(result);
+    }
+    return res.json(result);
+
+});
+router.get("/qrcode", async (req, res, next) => {
+    var result = await client.getState();
+    if (!result) {
+	res.json("QRCODENAOEXPORTADO");
+    }
+	res.json("QRCODELIDO");
 });
 
 router.listen(port, () => {
